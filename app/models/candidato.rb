f2c5ba39c0e452748d68982509d63b73930a8054
Adalberto Júnior <adalbertorsilvajr@gmail.com>
2015-12-08 18:00:50 -0300

    ajustes de estilo
    ajustes no caminho onde as fotografias são salvas e no estilo das
    paginas

--- app/models/candidato.rb
+++ app/models/candidato.rb
@@ -3,7 +3,7 @@ class Candidato < ActiveRecord::Base
   validates :idade, numericality: {only_integer: true}
   validates :peso, :altura, numericality: true
 
-  has_attached_file :fotografia, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: ":rails_root/public/:attachment/:filename"
+  has_attached_file :fotografia, styles: { medium: "200x200>" }, default_url: ":rails_root/public/:filename"
   validates_attachment :fotografia, presence: {message: "Por favor insira uma fotografia própria"}
   validates_attachment :fotografia, content_type: { content_type: "image/jpeg" }
 end
